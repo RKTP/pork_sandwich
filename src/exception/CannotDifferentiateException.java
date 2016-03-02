dda9659be09e5eedf7ffd30bb784c2d581a934a9
jediweon <archetype93@gmail.com>
2016-03-02 14:33:50 +0900

    [Implement] Derivative and range exception added

---
+++ src/exception/CannotDifferentiateException.java
@@ -0,0 +1,6 @@
+package exception;
+
+public class CannotDifferentiateException extends Exception {
+	private static final long serialVersionUID = 7968470926665069927L;
+
+}
---
+++ src/exception/RangeException.java
@@ -0,0 +1,6 @@
+package exception;
+
+public class RangeException extends Exception {
+	private static final long serialVersionUID = 3626842506016978292L;
+
+}
