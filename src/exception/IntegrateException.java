dde41383ed902844f92f7ba114f43c94d1986b27
jediweon <archetype93@gmail.com>
2016-03-02 09:41:06 +0900

    [Implement] Serialzation key added on customized Exception classes

--- src/exception/IntegrateException.java
+++ src/exception/IntegrateException.java
@@ -1,5 +1,6 @@
 package exception;
 
 public class IntegrateException extends Exception {
+	private static final long serialVersionUID = -922713712946770776L;
 
 }
--- src/exception/ValueException.java
+++ src/exception/ValueException.java
@@ -1,4 +1,5 @@
 package exception;
 
 public class ValueException extends Exception {
+	private static final long serialVersionUID = -735910073988166791L;
 }
