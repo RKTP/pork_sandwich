4a1b14918d2ec1d307be7f3c82f92e6b623711b8
jediweon <archetype93@gmail.com>
2016-03-02 13:21:08 +0900

    [Chore] whitespace, and main func.

--- src/exception/ValueException.java
+++ src/exception/ValueException.java
@@ -2,4 +2,5 @@ package exception;
 
 public class ValueException extends Exception {
 	private static final long serialVersionUID = -735910073988166791L;
+	
 }
--- src/userInterface/MockInterface.java
+++ src/userInterface/MockInterface.java
@@ -1,5 +1,7 @@
 package userInterface;
 
 public class MockInterface {
-
+	public static void main(String[] args) {
+		
+	}
 }
