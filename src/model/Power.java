783b195fad69cde774fa153799b40542a9b4e813
jediweon <archetype93@gmail.com>
2016-03-03 16:02:09 +0900

    [Implement] Polynomial class has been merged into Power class

---
+++ src/model/Power.java
@@ -0,0 +1,43 @@
+package model;
+
+import java.lang.Math;
+
+public class Power extends Expression {
+	Calculatable variable;
+	Expression power;
+	
+	public Power(Calculatable variable, Expression power) {
+		this.variable = variable;
+		this.power = power;
+		
+	}
+	
+	@Override
+	public Expression derivative(Variable var) {
+		if(!(this.variable instanceof Expression)) {
+			return new Multiply(this.power, new Power(this.variable, new Subtract(this.power, new Value(1.0))));
+		} else {
+			return null;//function derivative
+		}
+	}
+
+	@Override
+	public Expression integrate(Variable var) {
+		if(!(this.variable instanceof Expression)) {
+			return new Divide(new Power(this.variable, new Add(this.power, new Value(1.0))), new Add(this.power, new Value(1.0)));
+		} else {
+			return null;//function integration
+		}
+	}
+
+	@Override
+	public double calc() throws Exception {
+		return Math.pow(this.variable.calc(), this.power.calc());
+	}
+
+	@Override
+	public String stringify() {
+		return this.variable.stringify() + "^" + this.power.stringify();
+	}
+
+}
