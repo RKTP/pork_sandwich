68950871bf58be390ec127589dcbf5ff76086b8f
archetype <archetype93@gmail.com>
2016-03-06 23:47:29 +0900

    [Test] Removing non using test class / and implemented special constant test class

--- src/unitTest/EulerTest.java
+++ src/unitTest/EulerTest.java
@@ -2,18 +2,26 @@ package unitTest;
 
 import static org.junit.Assert.*;
 
+import model.Euler;
+import model.Expression;
 import org.junit.Before;
 import org.junit.Test;
 
 public class EulerTest {
+	Expression euler;
 
 	@Before
 	public void setUp() throws Exception {
+		euler = new Euler();
 	}
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testCalc() throws Exception {
+		assertEquals(euler.calc() , Math.E, 0.0000001);
 	}
 
+	@Test
+	public void testderiv() throws Exception {
+		assertEquals(euler.derivative(null).calc(), 0.0, 0.0000001);
+	}
 }
--- src/unitTest/PiTest.java
+++ src/unitTest/PiTest.java
@@ -2,18 +2,27 @@ package unitTest;
 
 import static org.junit.Assert.*;
 
+import model.Expression;
+import model.Pi;
 import org.junit.Before;
 import org.junit.Test;
 
 public class PiTest {
+	Expression pi;
 
 	@Before
 	public void setUp() throws Exception {
+		pi = new Pi();
 	}
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testCalc() throws Exception {
+		assertEquals(pi.calc() , Math.PI, 0.0000001);
+	}
+
+	@Test
+	public void testderiv() throws Exception {
+		assertEquals(pi.derivative(null).calc(), 0.0, 0.0000001);
 	}
 
 }
