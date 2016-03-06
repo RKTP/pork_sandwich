fdc2fd1fc342cbfd25c0442ce86a44920f468b95
archetype <archetype93@gmail.com>
2016-03-06 23:47:56 +0900

    [Implement] Function class which integrates all functionalities

--- src/model/Function.java
+++ src/model/Function.java
@@ -1,16 +1,49 @@
 package model;
 
-import java.util.ArrayList;
+import exception.VariableAlreadyExistsException;
+
+import java.util.*;
 
 public class Function {
-	private ArrayList<Variable> varList;
+	private Map<Character, Variable> varMap;
 	private Expression formula;
 
-	/*
-	 * mock constructor
-	 */
-	public Function(ArrayList<Variable> varList, Expression formula) {
-		this.varList = varList;
+	public Function(Set<Variable> varList, Expression formula) throws Exception {
 		this.formula = formula;
+		this.varMap = new HashMap<>();
+		Iterator<Variable> it = varList.iterator();
+
+		for(;it.hasNext();) {
+			Variable v = it.next();
+			if(this.varMap.containsKey(v.getName())) {
+				throw new VariableAlreadyExistsException();
+			} else {
+				this.varMap.put(v.getName(),v);
+			}
+		}
 	}
+
+	public void setVariableValue(char key, double value) {
+		Variable var = this.varMap.get(key);
+		var.setValue(value);
+	}
+
+	public ArrayList<Expression> getDerivative() throws Exception {
+		Set<Character> keySet = this.varMap.keySet();
+		Iterator<Character> it = keySet.iterator();
+		ArrayList<Expression> derivativeList = new ArrayList<>();
+
+		for(;it.hasNext();) {
+			Character key = it.next();
+			derivativeList.add(this.formula.derivative(this.varMap.get(key)));
+		}
+
+		return derivativeList;
+	}
+
+	public Expression getDerivative(Variable var) throws Exception {
+		return this.formula.derivative(var);
+	}
+
+
 }
