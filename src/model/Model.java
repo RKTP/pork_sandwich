cddc9e9f2fe9b8ad4849230a533dd4364b0db89e
jediweon <archetype93@gmail.com>
2016-03-09 22:57:30 +0900

    [Implement] Model will be head class of all model functionalities

--- src/model/Model.java
+++ src/model/Model.java
@@ -1,5 +1,19 @@
 package model;
 
+import java.util.ArrayList;
+
 public class Model {
+	private ArrayList<Function> functionList;
 	
+	public Model() {
+		this.functionList = new ArrayList<>();
+	}
+	
+	public void addFunction(Function func) {
+		this.functionList.add(func);
+	}
+	
+	public Function getFunction(int index) {
+		return this.functionList.get(index);
+	}
 }
