7827c4635b39ca878d90a15bafe977ecbaf8912a
jediweon <archetype93@gmail.com>
2016-03-10 22:59:50 +0900

    [Implement] Demo added

--- src/Application.java
+++ src/Application.java
@@ -2,12 +2,15 @@ import java.util.ArrayList;
 
 import model.*;
 import userInterface.GraphInterface;
+import userInterface.MainGraphicInterface;
 
 public class Application {
 
 	public static void main(String[] args) throws Exception {
 		Function func;
 		Variable x = new Variable('x',8.0);
+		Variable y = new Variable('y',2.0);
+		Variable z = new Variable('z',-3.0);
 		Expression x_var = new Power(x,new Constant(1.0));
 		ArrayList<Expression> add = new ArrayList<>();
 		add.add(new Power(x,new Constant(3.0),2));
@@ -46,7 +49,7 @@ public class Application {
 		add.add(new Power(x,new Constant(1.0),6));
 		add.add(new Constant(2.0));
 		exp = new Add(add);
-		func = new Function(exp);
+		func = new Function(exp, "testFunc");
 		new GraphInterface(func, -5, 5).setVisible(true);
 		
 		Function iFunc = func.getIntegra('x');
@@ -55,7 +58,29 @@ public class Application {
 		Function idFunc = iFunc.getDerivative('x');
 		new GraphInterface(idFunc,-5,5).setVisible(true);
 		
+		add.removeAll(add);
 		
+		add.add(new Power(x, new Constant(3.0)));
+		add.add(new Power(y, new Constant(2.0),-2.0));
+		add.add(new Power(z, new Constant(1.0),1.5));
+		exp = new Add(add);
+		func = new Function(exp);
+		
+		ArrayList<Function> funcList = func.getDerivative();
+		
+		for(Function e : funcList) {
+			System.out.println(e.stringify());
+		}
+		
+		//new MainGraphicInterface(new Model()).setVisible(true);
+		
+	}
+	
+	private void showDemo(Function func) {
+		if(func.getVarNames().size()==1) {
+			new GraphInterface(func, -10,10).setVisible(true);
+		} else {
+		}
 	}
 
 }
