1bf76f753b610c1a1b19f74a8b8c2f34ecaeb9cd
archetype <archetype93@gmail.com>
2016-03-16 23:18:54 +0900

    [Implement] Parse tree design will be composed with these classes

---
+++ src/parser/ParsedNode.java
@@ -0,0 +1,10 @@
+package parser;
+
+import model.Expression;
+
+/**
+ * Created by Julian on 2016-03-16.
+ */
+public interface ParsedNode {
+    public Expression functionize();
+}
---
+++ src/parser/Term.java
@@ -0,0 +1,7 @@
+package parser;
+
+/**
+ * Created by Julian on 2016-03-16.
+ */
+public class Term {
+}
